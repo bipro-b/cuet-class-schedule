a9493d306ec9d81a160449dea9162f774d2fbf80
SMSristi <u1804062@student.cuet.ac.bd>
2023-12-12 16:00:59 +0600

    modi

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+
+    return(
+        <>
+        <h1>Navbar</h1>
+        <p>home login</p>
+        <li>Login</li>
+
+        <h2>Hi </h2>
+        </>
+    )
+}
+
+export default Navbar;
