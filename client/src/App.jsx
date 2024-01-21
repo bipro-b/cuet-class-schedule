dfc3b134b81cdbf60912972aca629584955314ee
bipro <biprocuet@gmail.com>
2024-01-21 22:15:49 +0600

    redux setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
+
+import {BrowserRouter, Routes, Route}  from 'react-router-dom'
+import Home from "./pages/Home/Home";
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-      <h1>Start Code </h1>
-      {/* 
-      Routes
+    
   <BrowserRouter>
-  <Header/>   common
+  <Navbar/>  
   <Routes>
     <Route path='/' element={<Home/>} />
-    <Route path='/sign-in' element={<SignIn/>} />
+     {/* <Route path='/sign-in' element={<SignIn/>} />
     <Route path='/sign-up' element={<SignUp/>} />
     <Route path='/about' element={<About/>} />
     <Route element={< PrivateRoute/>} >
-    <Route path='/profile' element={<Profile/>} />
-    </Route>
+    <Route path='/profile' element={<Profile/>} /> 
+    </Route> */}
   </Routes>
   </BrowserRouter>
-     
-      */}
+    
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-/* 
-Redux connections
+/*
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes.jsx'; // it may changes may app
 import { Provider } from 'react-redux';
 import store from './redux/store';
+*/
 
-Another way
 
 import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store.js'
-import { PersistGate } from 'redux-persist/integration/react'
-*/
+import store from "./redux/store.js";
+
+// import { PersistGate } from 'redux-persist/integration/react'
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
@@ -26,13 +26,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </React.StrictMode>
      */}
 
-    {/* 
-    Another way
-    <Provider store={store}>
-     <PersistGate loading ={null} persistor={persistor}>
-      </PersistGate>
-      <App/>
-    </Provider>
-      */}
+  <Provider store={store}>
+    <App />
+  </Provider>
+  
   </React.StrictMode>
 );
