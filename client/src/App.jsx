0f55a038a6ad59efe0a7b63ec637a187f6775c24
Niaj_Mahmud_Pial <niajpial30@gmail.com>
2024-01-23 23:32:12 +0600

    course coordinator form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,14 @@
 import {BrowserRouter, Routes, Route}  from 'react-router-dom'
 import Home from "./pages/Home/Home";
 import Navbar from './components/Navbar';
-
+import CourseCoordinator from './pages/Coordinator/CourseAssign/CourseAssign';
 function App() {
   return (
     <>
     
   <BrowserRouter>
-  <Navbar/>  
   <Routes>
-    <Route path='/' element={<Home/>} />
+    <Route path='/' element={<CourseCoordinator/>} />
      {/* <Route path='/sign-in' element={<SignIn/>} />
     <Route path='/sign-up' element={<SignUp/>} />
     <Route path='/about' element={<About/>} />
