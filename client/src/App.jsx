53de23c31a406abc0eff6bd43ea9a17980dc8a8a
Niaj_Mahmud_Pial <niajpial30@gmail.com>
2024-01-25 23:58:19 +0600

    coursecoordinator_dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import {BrowserRouter, Routes, Route}  from 'react-router-dom'
 import Home from "./pages/Home/Home";
 import Navbar from './components/Navbar';
 import CourseCoordinator from './pages/Coordinator/CourseAssign/CourseAssign';
+import Dashboard from './pages/Coordinator/Dashboard/Dashboard';
 function App() {
   return (
     <>
     
   <BrowserRouter>
   <Routes>
-    <Route path='/' element={<CourseCoordinator/>} />
+    <Route path='/' element={<Dashboard/>} />
+    <Route path='/form_submit' element={<CourseCoordinator/>} />
      {/* <Route path='/sign-in' element={<SignIn/>} />
     <Route path='/sign-up' element={<SignUp/>} />
     <Route path='/about' element={<About/>} />
